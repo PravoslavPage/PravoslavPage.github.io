33d6c13b793b1228100dc7f6f27e2cfe5e67bb47
PravoslavPage <58198263+PravoslavPage@users.noreply.github.com>
2019-11-26 09:16:44 +0700

    init commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+	$('.slider_inner').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		nextArrow: '<button type="button" class="slick_btn slick-next"></button>',
+		prevArrow: '<button type="button" class="slick_btn slick-prev"></button>',
+	});
+
+});
